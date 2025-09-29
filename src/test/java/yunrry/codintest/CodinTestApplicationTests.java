0bea34f06d23eaf214983acb110a79a46c9d0271
yunrry <yunryy329@gmail.com>
2025-09-29 15:21:24 +0900

    init

---
+++ src/test/java/yunrry/codintest/CodinTestApplicationTests.java
@@ -0,0 +1,13 @@
+package yunrry.codintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodinTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
